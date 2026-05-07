07b9e3a6cd0212331e9670867707c98cde84d038
andreeliasgv <andrevila2016@hotmail.com>
2026-05-07 20:42:02 -0300

    Adicionado projeto TabuadaFOR

---
+++ C#/TabuadaFOR/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("TABUADA");
+        Console.WriteLine("Digite um número para exibir a tabuada: ");
+        Console.Write("> ");
+        
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine($"TABUADA DO {numero}:");
+
+        Console.WriteLine();
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine($"{i} x {numero} = {i * numero}");
+        }
+        Console.WriteLine();
+    }
+}
